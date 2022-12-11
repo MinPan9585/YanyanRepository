72133e594b6d29f74f200a6bfecc4653dd6ba986
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2022-12-11 22:05:06 +0800

    upload new scene and material and script 1211

--- YanyanProject/Assets/Scripts/Player.cs
+++ YanyanProject/Assets/Scripts/Player.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    //bool exploded = false;
+    public GameObject particle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(0, 0, 0.01f);
+        //transform.position = transform.position + new Vector3(0, 0, 0.01f);
+        //if(exploded == false)
+        //{
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5f);
+        //}
+        
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -23,5 +32,19 @@ public class Player : MonoBehaviour
         {
             transform.position = transform.position + new Vector3(0.01f, 0, 0);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //exploded = true;
+        //GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -50), ForceMode.Impulse);
+        Destroy(other.gameObject);
+        particle.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Time.timeScale = 0;
+        SceneManager.LoadScene("SampleScene");
     }
 }
